e980ef8e43c16631492864d108a270ec3427cc15
giannisaladino <saladinogianni6@gmail.com>
2024-03-25 15:30:47 +0100

    ADD: first-vueJS

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'High-quality Gattino png',
+            srcImage: "./cute-young-cat-ai-generative-free-png.webp",
+        }
+    }
+}).mount('#app')
